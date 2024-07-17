8362ec40dfa9a91a7c0b386354cf7edc9e6fb524
cmonge76 <monge_carlos76@hotmail.com>
2024-07-17 16:45:58 +0000

    3cambio en archivo comments.js

--- comments.js
+++ comments.js
@@ -1,4 +1,28 @@
 //create web server
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'comments'
+});
+connection.connect();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.get('/comments', function(req, res) {
+  connection.query('SELECT * FROM comments', function(err, rows){
+    res.json(rows);
+  });
+});
+app.post('/comments', function(req, res) {
+  var comment = req.body;
+  connection.query('INSERT INTO comments SET ?', comment, function(err, result){
+    if (err) throw err;
+    res.send('Success');
+  });
+});
+app.listen(3000);
